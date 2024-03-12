ec57bab224c0be96806f3a20156dda390fe68f96
j.jauregui@uniandes.edu.co <j.jauregui@uniandes.edu.co>
2024-03-12 09:22:00 -0500

    Cambios Ejercicio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from './es-Messages'; 
+import localeEnMessages from './en-Messages'; 
+import JobsList from './JobsList'; 
 
-import JobsList from "./components/jobslist";
+const userLanguage = navigator.language || navigator.userLanguage;
+const messages = userLanguage.startsWith('es') ? localeEsMessages : localeEnMessages;
+const locale = userLanguage;
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={messages}>
+    <JobsList />
+  </IntlProvider>, 
+  document.getElementById("root")
+);
+
